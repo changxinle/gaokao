9694a9c212d60fa4736878f07e9d10b5b2666bc5
xinle <glymail@qq.com>
2019-06-27 17:17:55 +0800

    init

---
+++ configs/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+
+  //    数据库配置
+  URL: 'mongodb://127.0.0.1:27017/gaokao',
+  DB: 'gaokao',
+  HOST: '127.0.0.1',
+  PORT: 27017,
+  USERNAME: 'root',
+  PASSWORD: 'root',
+
+  // redis配置
+  openRedis: true, // 测试或生产环境必须开启
+  redis_host: '127.0.0.1',
+  redis_port: 6379,
+  redis_psd: 'your redis password',
+  redis_db: 0,
+  redis_ttl: 12, // 12 小时
+
+};
